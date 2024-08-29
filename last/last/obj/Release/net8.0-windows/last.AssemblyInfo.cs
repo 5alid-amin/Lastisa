4faf4461a2c5ba7dc90b72e802c00da3e44b146b
5aled <5aled.amin.sn@gmail.com>
2024-08-30 02:37:05 +0300

    first setup commit

--- last/last/obj/Release/net8.0-windows/last.AssemblyInfo.cs
+++ last/last/obj/Release/net8.0-windows/last.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("last")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b4d47eb255c3eee26501e38763258430fc14a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("last")]
